ebf06983b0f67421aecf650d30b5bb7b3e3d9657
goldeneyezhang <goldeneyezhang@163.com>
2017-12-08 12:09:46 +0800

    windsor

---
+++ ConsoleAppWindsor/MyMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWindsor
+{
+	public class MyMain
+	{
+		private IDependency1 object1;
+		private IDependency2 object2;
+
+		public MyMain(IDependency1 dependency1,IDependency2 dependency2)
+		{
+			object1 = dependency1;
+			object2 = dependency2;
+		}
+		public void DoSomething()
+		{
+			object1.SomeObject = "Hello World";
+			object2.SomeOtherObject = "Hello Mars";
+			Console.WriteLine(object1.SomeObject);
+			Console.WriteLine(object2.SomeOtherObject);
+		}
+	}
+}
---
+++ ConsoleAppWindsor/Program.cs
@@ -0,0 +1,26 @@
+﻿using Castle.Windsor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWindsor
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			//create a windsor container object and register the interfaces, AND THEIR CONCRETE IMPLEMENTATIONS.
+			var container = new WindsorContainer();
+			container.Register(Castle.MicroKernel.Registration.Component.For<MyMain>());
+			container.Register(Castle.MicroKernel.Registration.Component.For<IDependency1>().ImplementedBy<Dependency1>());
+			container.Register(Castle.MicroKernel.Registration.Component.For<IDependency2>().ImplementedBy<Dependency2>());
+			//create the main object and invoke its method as desired
+			var mainThing = container.Resolve<MyMain>();
+			mainThing.DoSomething();
+			Console.Read();
+		}
+	}
+}
